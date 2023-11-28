177bdbd618f1a0fe58a509688dd96c33f6e73148
jannathh <muheenjannath@gmail.com>
2023-11-28 17:20:37 +0400

    Broken_grades.py file fixed

--- broken_grades.py
+++ broken_grades.py
@@ -21,36 +21,37 @@
 
 exam_one = int(input("Input exam grade one: "))
 
-exam_two = input("Input exam grade two: "))
+exam_two = int(input("Input exam grade two: ")) #added "int" before input because the input taken in is an integer
 
-exam_3 = str(input("Input exam grade three: "))
+exam_three = int(input("Input exam grade three: ")) #changed the variable name exam_3 to exam_three to match the previously assigned varibale names 
+#also changed "str" to "int" because the input taken in is an integer and not a string
 
-grades = [exam_one exam_two exam_three]
+grades = [exam_one, exam_two, exam_three] #added commas to seperate the values in the list
 sum = 0
-for grade in grade:
+for grade in grades: #changed "grade" to "grades"
   sum = sum + grade
 
-avg = sum / len(grdes)
+avg = sum / len(grades) #spelling error corrected to "grades"
 
 if avg >= 90:
     letter_grade = "A"
-elif avg >= 80 and avg < 90
+elif avg >= 80 and avg < 90: #added missing colon
     letter_grade = "B"
 elif avg > 69 and avg < 80:
-    letter_grade = "C'
-elif avg <= 69 and avg >= 65:
+    letter_grade = "C" #changed the single quote to double quotes at the end
+elif avg >= 69 and avg > 70: #corrected the logical error; changed "<= 69" to ">= 69", and ">= 65" to "< 70" 
     letter_grade = "D"
-elif:
+else: #changed "elif" to "else"
     letter_grade = "F"
 
-for grade in grades:
-    print("Exam: " + str(grade))
+print("Exam: " + str(exam_one) + ", ", str(exam_two), ", ", str(exam_three))
 
-    print("Average: " + str(avg))
+print("Average: " + str(avg))
 
-    print("Grade: " + letter_grade)
+print("Grade: " + letter_grade)
 
-if letter-grade is "F":
-    print "Student is failing."
+if letter_grade == "F": #replaced hyphen with underscore for letter_grade
+#also changed "is" to "=="
+    print ("Student is failing.") #added missing brackets
 else:
-    print "Student is passing."
+    print ("Student is passing.") #added missing brackets
